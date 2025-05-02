404f7069b4907b6ab45eff0b984ec1edc78b5792
sughimura <nxsughi@gmail.com>
2025-05-02 21:14:12 +0900

    feat: add complex_square_add_loop

--- examples/mandelbrot/src/main.rs
+++ examples/mandelbrot/src/main.rs
@@ -1,7 +1,9 @@
-fn square_loop(mut c: f64) {
-    let mut x = 0.;
+use num::Complex;
+
+fn complex_square_add_loop(mut c: Complex<f64>) {
+    let mut z = Complex { re: 0.0, im: 0.0 };
     loop {
-        x = x * x + c;
+        z = z * z + c;
     }
 }
 
