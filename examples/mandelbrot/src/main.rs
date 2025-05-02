0d2c4d794db1bd7a87d64660b9fee7d027149ef2
sughimura <nxsughi@gmail.com>
2025-05-02 21:19:34 +0900

    feat: add escape_time

--- examples/mandelbrot/src/main.rs
+++ examples/mandelbrot/src/main.rs
@@ -1,5 +1,16 @@
 use num::Complex;
 
+fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    for i in 0..limit {
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+        z = z * z + c;
+    }
+    None
+}
+
 fn complex_square_add_loop(mut c: Complex<f64>) {
     let mut z = Complex { re: 0.0, im: 0.0 };
     loop {
