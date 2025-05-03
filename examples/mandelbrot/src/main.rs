3c75a9e97d0ddfc4d40943a37d1db995412f7f03
sughimura <nxsughi@gmail.com>
2025-05-03 14:20:51 +0900

    feat: add write_image

--- examples/mandelbrot/src/main.rs
+++ examples/mandelbrot/src/main.rs
@@ -100,6 +100,22 @@ fn render(pixels: &mut [u8],
     }
 }
 
+use image::ColorType;
+use image::png::PNGEncoder;
+use std::fs::File;
+
+/// 大きさが `bounds` で指定されたバッファ `pixels` を `filename` で指定されたファイルに書き出す
+fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> Result<(), std::io::Error>
+{
+    let output = File::create(filename)?;
+
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(pixels,
+                   bounds.0 as u32, bounds.1 as u32,
+                   ColorType::Gray(8))?;
+    Ok(())
+}
+
 fn complex_square_add_loop(mut c: Complex<f64>) {
     let mut z = Complex { re: 0.0, im: 0.0 };
     loop {
